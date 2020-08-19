26b2b84d8a43cc5346842885f8ccd71427102d72
Jakob Waibel <jakob.waibel@web.de>
2020-08-19 14:54:01 +0200

    first commit

---
+++ tutorial.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// only numbers, letters and underscores in the variable names
+	var name string = "Hello World!"
+	name = "Jakob"
+	name = "Paula"
+	fmt.Println(name)
+}
