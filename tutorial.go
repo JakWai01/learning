d6a12a3c532603d50b1ad74060395e4bb8553834
Jakob Waibel <jakob.waibel@web.de>
2020-08-26 20:00:13 +0200

    completing tutorial

--- tutorial.go
+++ tutorial.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"fmt"
+	"math"
+)
+
 /*
 func main() {
 
@@ -373,3 +378,38 @@ func (s Student) getAverageGrade() float32 {
 }
 
 */
+
+type shape interface {
+	area() float64
+}
+
+type circle struct {
+	radius float64
+}
+
+type rect struct {
+	width  float64
+	height float64
+}
+
+func (r *rect) area() float64 {
+	return r.width * r.height
+}
+
+func (c *circle) area() float64 {
+	return c.radius * c.radius * math.Pi
+}
+
+func getArea(s shape) float64 {
+	return s.area()
+}
+
+func main() {
+	c1 := circle{4.5}
+	r1 := rect{5, 7}
+	shapes := []shape{&c1, &r1}
+
+	for _, shape := range shapes {
+		fmt.Println(getArea(shape))
+	}
+}
