ae2e93477e287cef944869f6e06f6694acc1d88b
Jakob Waibel <jakob.waibel@web.de>
2020-08-27 11:46:45 +0200

    implementing log and net

--- main.go
+++ main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"bufio"
 	"flag"
 	"fmt"
+	"log"
+	"net"
 	"sync"
 )
 
@@ -23,14 +26,26 @@ func main() {
 	go substract(a, b, wg)
 
 	wg.Wait()
+
+	// net
+	conn, err := net.Dial("tcp", "golang.org:80")
+	if err != nil {
+		log.Fatal()
+	}
+	fmt.Fprintf(conn, "GET / HTTP/1.0\r\n\r\n")
+	status, err := bufio.NewReader(conn).ReadString('\n')
+	fmt.Println(status)
+
 }
 
 func add(a, b int, wg *sync.WaitGroup) int {
 	defer wg.Done()
+	log.Print("Additions has been done!")
 	return a + b
 }
 
 func substract(a, b int, wg *sync.WaitGroup) int {
 	defer wg.Done()
+	log.Print("Substraction has been done!")
 	return a - b
 }
