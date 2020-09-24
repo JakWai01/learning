4b05d18a256b04182b7d760642631e6470c97059
Jakob Waibel <jakob.waibel@web.de>
2020-09-24 10:31:25 +0200

    feat: Add basic db client

--- pages/api/todos.js
+++ pages/api/todos.js
@@ -1,6 +1,14 @@
-const todos = (req, res) => {
-    res.statusCode = 200
-    res.json({message: "Hello World"})
+import query from "../../lib/db";
+
+const todos = async (req, res) => {
+    const todos = await query `select * from todos;`
+
+    res.statusCode = 200;
+    res.json(todos.map((t) => ({
+        id: t.id,
+        title: t.title,
+        body: t.body
+    })));
 }
 
 export default todos;
