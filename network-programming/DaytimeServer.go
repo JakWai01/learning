6de7aa7317434d02dac0955e18056eb481edbed3
Jakob Waibel <jakob.waibel@web.de>
2020-10-02 00:01:16 +0200

    feat: TCP Sockets

---
+++ network-programming/DaytimeServer.go
@@ -0,0 +1,38 @@
+package main
+
+/*
+DaytimeServer
+*/
+
+import (
+	"fmt"
+	"net"
+	"os"
+	"time"
+)
+
+func main() {
+
+	service := ":1200"
+
+	tcpAddr, err := net.ListenTCP("tcp", tcpAddr)
+	checkError(err)
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			continue
+		}
+
+		daytime := time.Now().String()
+		conn.Write([]byte(daytime))
+		conn.Close()
+	}
+}
+
+func CheckError() {
+	if err != nil {
+		fmt.Println(os.Stderr, "Fatal error: %s", err.Error())
+		os.Exit(1)
+	}
+}
--- network-programming/LookupHost.go
+++ network-programming/LookupHost.go
@@ -1,34 +1,34 @@
-package main
+// package main
 
-/*
-LookupHost
-*/
+// /*
+// LookupHost
+// */
 
-import (
-	"fmt"
-	"net"
-	"os"
-)
+// import (
+// 	"fmt"
+// 	"net"
+// 	"os"
+// )
 
-func main() {
-	if len(os.Args) != 2 {
-		fmt.Fprintf(os.Stderr, "Usage: %s hostname\n", os.Args[0])
-		os.Exit(1)
-	}
+// func main() {
+// 	if len(os.Args) != 2 {
+// 		fmt.Fprintf(os.Stderr, "Usage: %s hostname\n", os.Args[0])
+// 		os.Exit(1)
+// 	}
 
-	name := os.Args[1]
+// 	name := os.Args[1]
 
-	addrs, err := net.LookupHost(name)
+// 	addrs, err := net.LookupHost(name)
 
-	if err != nil {
-		fmt.Println("Error: ", err.Error())
+// 	if err != nil {
+// 		fmt.Println("Error: ", err.Error())
 
-		os.Exit(2)
-	}
+// 		os.Exit(2)
+// 	}
 
-	for _, s := range addrs {
-		fmt.Println(s)
-	}
+// 	for _, s := range addrs {
+// 		fmt.Println(s)
+// 	}
 
-	os.Exit(0)
-}
+// 	os.Exit(0)
+// }
