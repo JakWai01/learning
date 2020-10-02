59c8ff57e8e69449fab65a8425ace0f3c6df9a31
Jakob Waibel <jakob.waibel@web.de>
2020-10-02 20:27:52 +0200

    feat: implementing multiple files

--- network-programming/UDPDaytimeClient.go
+++ network-programming/UDPDaytimeClient.go
@@ -1,46 +1,45 @@
-/*
-UDPDaytimeClient
-*/
+// /*
+// UDPDaytimeClient
+// */
 
-package main
+// package main
 
-import (
-	"fmt"
-	"log"
-	"net"
-	"os"
-)
+// import (
+// 	"fmt"
+// 	"net"
+// 	"os"
+// )
 
-func main() {
+// func main() {
 
-	if len(os.Args) != 2 {
-		fmt.Fprintf("Usage of: %s host:port", os.Args[0])
-		os.Exit(1)
-	}
+// 	if len(os.Args) != 2 {
+// 		fmt.Fprintf("Usage of: %s host:port", os.Args[0])
+// 		os.Exit(1)
+// 	}
 
-	service := os.Args[1]
+// 	service := os.Args[1]
 
-	udpAddr, err := net.ResolveUDPAddr("up4", service)
-	checkError(err)
+// 	udpAddr, err := net.ResolveUDPAddr("up4", service)
+// 	checkError(err)
 
-	conn, err := net.DialUDP("udp", nil, updAddr)
-	checkError(err)
+// 	conn, err := net.DialUDP("udp", nil, updAddr)
+// 	checkError(err)
 
-	_, err := conn.Write([]byte("anything"))
-	checkError(err)
+// 	_, err := conn.Write([]byte("anything"))
+// 	checkError(err)
 
-	var buf [512]byte
-	n, err := conn.Read(buf[0:])
-	checkError(err)
+// 	var buf [512]byte
+// 	n, err := conn.Read(buf[0:])
+// 	checkError(err)
 
-	fmt.Println(string(buf[0:n]))
+// 	fmt.Println(string(buf[0:n]))
 
-	os.Exit(0)
+// 	os.Exit(0)
 
-}
+// }
 
-func checkError(err error) {
+// func checkError(err error) {
 
-	log.Fatal(err)
-	os.Exit(1)
-}
+// 	log.Fatal(err)
+// 	os.Exit(1)
+// }
