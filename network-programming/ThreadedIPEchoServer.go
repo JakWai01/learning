edbdf357643550484c9244314614024eb229c03b
Jakob Waibel <jakob.waibel@web.de>
2020-10-02 20:44:36 +0200

    feat: implemeting ping

--- network-programming/ThreadedIPEchoServer.go
+++ network-programming/ThreadedIPEchoServer.go
@@ -1,50 +1,50 @@
-/* 
-ThreadedIPEchoServer
-*/
+// /* 
+// ThreadedIPEchoServer
+// */
 
-package main
+// package main
 
-import (
+// import (
 
-)
+// )
 
-func main() {
+// func main() {
 
-	service := ":1200"
-	listener, err := net.Listen("tcp", service)
-	checkError(err)
+// 	service := ":1200"
+// 	listener, err := net.Listen("tcp", service)
+// 	checkError(err)
 
-	for {
-		conn, err := listener.Accept()
-		if err != nil {
-			continue
-		}
+// 	for {
+// 		conn, err := listener.Accept()
+// 		if err != nil {
+// 			continue
+// 		}
 
-		go handleClient(conn)
-	}
+// 		go handleClient(conn)
+// 	}
 
-}
+// }
 
-func handleClient(conn net.Conn) {
-	defer conn.Close
+// func handleClient(conn net.Conn) {
+// 	defer conn.Close
 
-	var buf [512]byte
+// 	var buf [512]byte
 
-	for {
-		n, err := conn.Read(buf[0:])
-		if err != nil {
-			return
-		}
+// 	for {
+// 		n, err := conn.Read(buf[0:])
+// 		if err != nil {
+// 			return
+// 		}
 
-		_, err2 := conn.Write(buf[0:n])
-		if err2 != nil {
-			return
-		}
-	}
-}
+// 		_, err2 := conn.Write(buf[0:n])
+// 		if err2 != nil {
+// 			return
+// 		}
+// 	}
+// }
 
-func checkError(err error) {
+// func checkError(err error) {
 	
-	Log.fatal(err)
-	os.Exit(1)
-}
+// 	Log.fatal(err)
+// 	os.Exit(1)
+// }
