7aebbd8523d9b1e08c85c8d3cdf233eaf4f4aa17
Jakob Waibel <jakob.waibel@web.de>
2020-10-02 19:31:17 +0200

    feat: implementing UDPDaytimeServer

---
+++ network-programming/UDPDaytimeServer.go
@@ -0,0 +1,46 @@
+package main
+
+/*
+UDPDaytimeServer
+*/
+
+import (
+	"log"
+	"net"
+	"time"
+)
+
+func main() {
+
+	service := ":1200"
+
+	udpAddr, err := net.ResolveUDPAddr("up4", service)
+	checkError(err)
+
+	conn, err := net.ListenUDP("udp", udpAddr)
+	checkError(err)
+
+	for {
+		handleClient()
+	}
+}
+
+func handleClient() {
+
+	var buf [512]byte
+
+	_, arr, err := conn.ReadFromUDP(buf[0:])
+
+	if err != nil {
+		return
+	}
+
+	daytime := time.Now().String()
+
+	conn.WriteToUDP([]byte(daytime), addr)
+}
+
+func checkError(err error) {
+
+	log.Fatal(err)
+}
