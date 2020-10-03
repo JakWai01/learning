c90e4632219b04134dd4279dab34cd93e5e3c45b
Jakob Waibel <jakob.waibel@web.de>
2020-10-03 17:07:22 +0200

    fix: include "constructor" and open function

--- pkg/client/tcp.go
+++ pkg/client/tcp.go
@@ -2,28 +2,34 @@
 tcpClient
 */
 
-package main
+package client
 
 import (
-	"flag"
 	"fmt"
-	"log"
 	"net"
 	"os"
 )
 
-func main() {
-	laddr := flag.String("laddr", "0.0.0.0:8080", "TCP address")
+// TCPServer consists of laddr
+type TCPServer struct {
+	laddr string
+}
 
-	service := *laddr
+// NewTCPServer initializes laddr
+func NewTCPServer(laddr string) string {
+	return laddr
+}
 
-	tcpAddr, err := net.ResolveTCPAddr("tcp", service)
+// Open initializes TCP server with laddr
+func (s *TCPServer) Open() error {
+
+	tcpAddr, err := net.ResolveTCPAddr("tcp", s.laddr)
 	checkError(err)
 
 	conn, err := net.DialTCP("tcp", nil, tcpAddr)
 	checkError(err)
 
-	_, err2 := conn.Write([]byte("anything"))
+	_, err2 := conn.Write([]byte("Hello World"))
 	checkError(err2)
 
 	var buf [512]byte
@@ -35,11 +41,42 @@ func main() {
 
 	os.Exit(0)
 
+	return nil
+
 }
 
-func checkError(err error) {
-
-	if err != nil {
-		log.Fatal(err)
-	}
+func checkError(err error) error {
+	return err
 }
+
+// func main() {
+// 	laddr := flag.String("laddr", "0.0.0.0:8080", "TCP address")
+
+// 	service := *laddr
+
+// 	tcpAddr, err := net.ResolveTCPAddr("tcp", service)
+// 	checkError(err)
+
+// 	conn, err := net.DialTCP("tcp", nil, tcpAddr)
+// 	checkError(err)
+
+// 	_, err2 := conn.Write([]byte("anything"))
+// 	checkError(err2)
+
+// 	var buf [512]byte
+
+// 	n, err := conn.Read(buf[0:])
+// 	checkError(err)
+
+// 	fmt.Println(buf[0:n])
+
+// 	os.Exit(0)
+
+// }
+
+// func checkError(err error) {
+
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+// }
