58b80ee55f567542ac247e755692d4e428afb01b
Jakob Waibel <jakob.waibel@web.de>
2020-11-12 18:17:43 +0100

    feat: Add first graph

---
+++ matplotlib/graph.py
@@ -0,0 +1,6 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+fig, ax = plt.subplots() # Create figure containing a single axis 
+ax.plot([1,2,3,4],[1,4,2,3]) # Plot some data on the axes.
+plt.show()
