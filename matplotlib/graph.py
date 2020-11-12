f6cea4c7b5dcbe000fe059e6281fae298b24a5f1
Jakob Waibel <jakob.waibel@web.de>
2020-11-12 20:12:16 +0100

    feat: Add csv reader to graph

--- matplotlib/graph.py
+++ matplotlib/graph.py
@@ -1,6 +1,36 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import csv
 
-fig, ax = plt.subplots() # Create figure containing a single axis 
-ax.plot([1,2,3,4],[1,4,2,3]) # Plot some data on the axes.
+day = []
+temp = []
+
+# CSV reader
+with open('data.csv') as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    line_count = 0
+    for row in csv_reader:
+        if line_count == 0:
+            #column_names = .join(row)
+            line_count += 1
+        else: 
+            day.append(int(row[0]))
+            temp.append(int(row[1]))
+            line_count += 1
+
+
+print(day)
+
+print(temp)
+
+# Check if Graph valid
+if len(day) != len(temp):
+    print("Graph not bijective.")
+
+# Graph 
+plt.plot(day,temp) # Plot some data on the axes.
+plt.xlabel('day')
+plt.ylabel('temperature')
 plt.show()
+
+
