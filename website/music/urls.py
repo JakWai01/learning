af9aabea8cf1f503ed95e577c9e0c51b8496f59a
JakWai01 <jakob.waibel@web.de>
2021-03-21 17:22:04 +0100

    feat: Add favorite option

--- website/music/urls.py
+++ website/music/urls.py
@@ -6,4 +6,5 @@ app_name = 'music'
 urlpatterns = [
    path('', views.index, name='index'),
    path('<int:album_id>/', views.detail, name='detail'),
+   path('<int:album_id>/favorite/', views.favorite, name='favorite'),
 ]
