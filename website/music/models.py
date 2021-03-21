9152f4aa1626ed254853b9ec1eb3768dcc0238f2
JakWai01 <jakob.waibel@web.de>
2021-03-21 12:12:49 +0100

    feat: Add database models

--- website/music/models.py
+++ website/music/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Album(models.Model):
+    artist = models.CharField(max_length=255)
+    album_title = models.CharField(max_length=511)
+    genre = models.CharField(max_length=127)
+    album_logo = models.CharField(max_length=1023)
+
+class Song(models.Model):
+    album = models.ForeignKey(Album, on_delete=models.CASCADE)
+    file_type = models.CharField(max_length=15)
+    song_title = models.CharField(max_length=255)
