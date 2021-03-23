22e61bc7f450e6e1ba9a8fd79f500e12d9a27357
JakWai01 <jakob.waibel@web.de>
2021-03-23 22:42:21 +0100

    feat: Defining model and displaying entry

--- todo/todoapp/views.py
+++ todo/todoapp/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import Todo
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello World")
+    return HttpResponse(str(Todo.objects.get(id=1)))
