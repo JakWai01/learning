bbabe927c0e8ac86f2c6167fdc6f8e988b39ddbf
JakWai01 <jakob.waibel@web.de>
2021-03-24 17:10:19 +0100

    feat: Remove checkbox

--- todo/todoapp/forms.py
+++ todo/todoapp/forms.py
@@ -2,5 +2,4 @@ from django import forms
 from django.forms import TextInput
 
 class CreateNewList(forms.Form):
-    text = forms.CharField(label="Text", max_length=200, widget=forms.TextInput(attrs={'class': 'focus:outline-none focus:ring focus:border-blue-300 rounded border-2'}))
-    check = forms.BooleanField(required=False)
+    text = forms.CharField(label="", max_length=200, widget=forms.TextInput(attrs={'class': 'focus:outline-none focus:ring focus:border-blue-300 rounded border-2 mb-2 w-max'}))
