bd0cf5e4d4bfb1f8812a1d13df0a53d224af5b9c
JakWai01 <jakob.waibel@web.de>
2021-07-23 14:48:27 +0200

    feat: Add conditionals, loops and optional execution of functions

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -1,3 +1,25 @@
+#![allow(warnings, unused)]
+mod conditionals;
+mod hello;
+mod loops;
+
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let key = "FUNC";
+
+    match env::var(key) {
+        Ok(val) => {
+            if val == "hello" {
+                hello::hello();
+            } else if val == "loops" {
+                loops::loops();
+            } else if val == "conditionals" {
+                conditionals::conditionals();
+            } else {
+                println!("There is no function called {} in this crate", val)
+            }
+        }
+        Err(e) => println!("couldn't interpret {}: {}", key, e),
+    }
 }
