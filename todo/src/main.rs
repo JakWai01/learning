c333a52a79356366973f8aea3f734a3a02673858
JakWai01 <jakob.waibel@web.de>
2021-07-25 14:27:59 +0200

    feat: Increment id's automatically

--- todo/src/main.rs
+++ todo/src/main.rs
@@ -8,6 +8,11 @@ struct Task {
     done: u32,
 }
 
+#[derive(Debug)]
+struct Count {
+    count: u32,
+}
+
 fn main() -> Result<()> {
     let conn = Connection::open("todo.db")?;
 
@@ -32,17 +37,25 @@ fn main() -> Result<()> {
     todos.insert(String::from("Code"), 1);
 
     // Get maximum ID instead
-    let mut count = 0;
+    let mut stmt = conn.prepare("SELECT max(t.id) from todos t;")?;
+
     for (task, done) in &todos {
+        let mut max_value = 0;
+
+        let values = stmt.query_map([], |row| Ok(Count { count: row.get(0)? }))?;
+        for val in values {
+            // println!("{:?}", val.unwrap().count);
+            let current_value = val.unwrap().count;
+            if current_value > max_value {
+                max_value = current_value + 1;
+            }
+        }
         conn.execute(
             "INSERT INTO todos (id, task, done) values (?1, ?2, ?3)",
-            &[&count.to_string(), &task, &done.to_string()],
+            &[&max_value.to_string(), &task, &done.to_string()],
         )?;
-
-        count += 1;
     }
     let mut stmt = conn.prepare("SELECT t.id, t.task, t.done from todos t;")?;
-
     let tasks = stmt.query_map([], |row| {
         Ok(Task {
             id: row.get(0)?,
