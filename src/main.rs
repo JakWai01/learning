0d9e3e9e358d3e2dcb9451029371e4516fd14c32
JakWai01 <jakob.waibel@web.de>
2021-08-11 11:44:06 +0200

    feat: Add prototype styling

--- src/main.rs
+++ src/main.rs
@@ -42,10 +42,18 @@ impl Component for Model {
 
     fn view(&self) -> Html {
         html! {
-            <div>
-                <h1>{String::from("Hello World!")}</h1>
-                <button onclick=self.link.callback(|_| Msg::AddOne)>{ "+1" }</button>
-                <p>{ self.value }</p>
+            <div class=classes!("h-screen", "flex", "justify-center")>
+                <div class=classes!("h-48", "m-auto", "flex", "items-center")>
+                    // <button onclick=self.link.callback(|_| Msg::AddOne)>{ "+1" }</button>
+                    // <p>{ self.value }</p>
+                    <form action="">
+                                // <label for="name" class="inline-block w-20 mr-6 text-right 
+                                //  font-bold text-gray-600">Name</label>
+                        <label for="name" class=classes!("text-3xl", "inline-block", "w-48", "mr-6", "text-right", "font-bold", "text-gray-600")>{"How bad in"}</label>
+                        <input placeholder="e.g. New York" class=classes!("mr-6", "text-3xl", "flex-1", "py-2", "border-b-2", "border-gray-400", "focus:border-green-400", "text-gray-600", "placeholder-gray-400", "outline-none") type="text" id="fname" name="fname"/>
+                        <label class=classes!("text-3xl")>{"?"}</label>
+                    </form>
+                </div>
             </div>
         }
     }
