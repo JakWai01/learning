7fe50e97057e3b386a3c60298ee001b7bc7054ee
JakWai01 <jakob.waibel@web.de>
2022-02-25 20:40:57 +0100

    refactor: Change to list based stack implementation

--- src/components/main.py
+++ src/components/main.py
@@ -6,6 +6,10 @@ text_input = read_file("../../samples/code.sucks")
 
 lexer = Lexer()
 tokens = lexer.lex(text_input)
+print(tokens)
 
 parser = Parser()
 parse_tree = parser.build_parse_tree(tokens)
+
+print(parse_tree.left.root)
+print(parser.eval(parse_tree))
--- src/components/parser.py
+++ src/components/parser.py
@@ -1,53 +1,32 @@
 import re
+from ast import Sub, Sum, BinaryOp, Number
 
 class ParseTree():
     def __init__(self, left=None, root=None, right=None):
         self.left = left 
         self.root = root
         self.right = right
-    
-class StackNode():
-    def __init__(self, value):
-        self.value = value 
-        self.next = None
 
-class Stack():
+# It's also possible using a linked list instead of implementing it with a list.
+# For the sake of brevity, we use a list. 
+class Stack:
     def __init__(self):
-        self.head = StackNode("head")
-        self.size = 0
-
-    def __str__(self):
-        cur = self.head.next
-        out = ""
-        while cur:
-            out += str(cur.value) + "->"
-            cur = cur.next
-        return out[:-3]
-
-    def getSize(self):
-        return self.size
+        self.items = []
 
     def isEmpty(self):
-        return self.size == 0
+        return self.items == []
 
-    def peek(self):
-        if self.isEmpty():
-            raise Exception("Peeking from an empty stack")
-        return self.head.next.value
-
-    def push(self, value):
-        node = StackNode(value)
-        node.next = self.head.next 
-        self.head.next = node
-        self.size += 1
+    def push(self, item):
+        self.items.append(item)
 
     def pop(self):
-        if self.isEmpty():
-            raise Exceptions("Popping from an empty stack")
-        remove = self.head.next
-        self.head.next = self.head.next.next
-        self.size -= 1
-        return remove.value
+        return self.items.pop()
+    
+    def peek(self):
+        return self.items[len(self.items)-1]
+    
+    def size(self):
+        return len(self.items)
 
 class Parser():
     def __init__(self: object):
@@ -63,19 +42,37 @@ class Parser():
         for token in tokens: 
             # print( 4 + 4 - 2 );
             if token == "(":
+                # 1 create tree on left side and go down
                 current_node.left = ParseTree()
                 stack.push(current_node)
                 current_node = current_node.left
             if token in ["+", "-"]:
+                # 3 set current root to "+", create tree on left and go there
+                # 5 set current root to "-", create tree on left and go there
                 current_node.root = token
                 current_node.right = ParseTree()
                 stack.push(current_node)
                 current_node = current_node.right
             if token.isdigit():
+                # 2 set value of tree to "4" and go to parent
+                # 4 set value of tree to "4" and go to parent
+                # 6 set value of tree to "2" and go to parent
                 current_node.root = int(token)
                 parent = stack.pop()
                 current_node = parent
             if token == ")":
-                stack.pop()
+                # 7 move to parent
+                current_node = stack.pop()
 
         return current_node
+
+    def eval(self, tree):
+        if tree.root == "+":
+            return Sum(BinaryOp(self.eval(tree.left), self.eval(tree.right))).eval()
+        if tree.root == "-":
+            return Sub(BinaryOp(self.eval(tree.left), self.eval(tree.right))).eval()
+        if type(tree.root) == type(4):
+            return int(tree.root)
+
+
+
